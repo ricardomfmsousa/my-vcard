b3103a5f976b096f8a3ffa61e0c32cd14d02bf5c
ricardomfmsousa <ricardomfmsousa@gmail.com>
2017-10-30 19:50:54 +0000

    Add components and routing to NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule, routedComponents } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routedComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
