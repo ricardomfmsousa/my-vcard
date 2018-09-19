421776450f6245f11516680bc1a5bc2848ba265c
Ricardo Sousa <r.sousa@techfirm.pt>
2018-09-19 23:18:43 +0100

    Remove video due to recent autostart browser security limitation

--- src/app/components/home-view/home-view.component.ts
+++ src/app/components/home-view/home-view.component.ts
@@ -7,4 +7,19 @@ import { fadeAnimation } from '../../animations/fade-in.animation';
   animations: [fadeAnimation],
   styleUrls: ['./home-view.component.scss']
 })
-export class HomeViewComponent {}
+export class HomeViewComponent {
+  public isPositionVisible: boolean;
+  public isSocialVisible: boolean;
+
+  public handleNameTypingComplete(): void {
+    window.setTimeout(() => {
+      this.isPositionVisible = true;
+    }, 1500);
+  }
+
+  public handlePositionTypingComplete(): void {
+    window.setTimeout(() => {
+      this.isSocialVisible = true;
+    }, 1500);
+  }
+}
