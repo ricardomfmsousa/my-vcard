438991795f4884701546ac7a4086fad3c326b8e2
Ricardo Sousa <r.sousa@techfirm.pt>
2018-10-05 12:29:40 +0100

    Add router testing module to unit test

--- src/app/views/not-found/not-found.component.spec.ts
+++ src/app/views/not-found/not-found.component.spec.ts
@@ -1,3 +1,4 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundViewComponent } from './not-found.component';
@@ -8,7 +9,8 @@ describe('NotFoundViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NotFoundViewComponent]
+      declarations: [NotFoundViewComponent],
+      imports: [RouterTestingModule]
     }).compileComponents();
   }));
 
