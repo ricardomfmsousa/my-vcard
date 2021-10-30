78c85c91f55cfaf70d86cca5d253b57a7bd900f2
Ricardo Sousa <r.sousa@techfirm.pt>
2021-10-30 17:59:51 +0100

    Update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Ricardo Sousa's vCard`,
+    description: `Simple presentation website with a subtle display of my development skills.`,
+    author: `ricardomfmsousa`,
+    siteUrl: `https://ricardomfmsousa.github.io/my-vcard/`,
   },
   plugins: [
     {
