a415dbfbb1520be351bd006f544ee2135fb7f3c3
Ricardo Sousa <r.sousa@techfirm.pt>
2022-08-25 00:20:18 +0100

    Replace example stories by page stories

---
+++ src/stories/404.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import { NotFoundPage as NotFoundPageComponent } from "../pages/404";
+
+export default {
+  title: "Design System/Pages/NotFoundPage",
+  component: NotFoundPageComponent,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof NotFoundPageComponent>;
+
+export const NotFoundPage: ComponentStory<typeof NotFoundPageComponent> = (
+  args
+) => <NotFoundPageComponent {...args} />;
