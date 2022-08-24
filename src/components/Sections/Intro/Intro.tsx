f3d0c4c809e76e1b4394b8cc7cca9ef47d46143f
Ricardo Sousa <r.sousa@techfirm.pt>
2022-08-24 22:41:10 +0100

    Optimize component code

--- src/components/Sections/Intro/Intro.tsx
+++ src/components/Sections/Intro/Intro.tsx
@@ -1,4 +1,5 @@
 import { Box, SxProps, Typography } from "@mui/material";
+import { useI18next } from "gatsby-plugin-react-i18next";
 import React from "react";
 import Typewriter from "typewriter-effect";
 
@@ -13,73 +14,88 @@ export interface IntroProps {
 export const Intro: React.FC<IntroProps> = ({
   introPadding,
   sx,
-}): JSX.Element => (
-  <Box
-    component="section"
-    sx={{
-      height: "100vh",
-      background: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)),
+}): JSX.Element => {
+  const { t } = useI18next();
+
+  return (
+    <Box
+      component="section"
+      sx={{
+        height: "100vh",
+        background: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)),
         url(https://images.pexels.com/photos/90764/man-studio-portrait-light-90764.jpeg)`,
-      backgroundColor: "",
-      backgroundSize: "cover",
-      backgroundPosition: {
-        xs: "62% center",
-        sm: "65% center",
-        md: "center center",
-      },
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      position: "relative",
-      textAlign: "center",
-      ...sx,
-    }}
-  >
-    <Typography variant="h1" fontSize="70px" fontWeight="700" mb={4} mx={4}>
-      Ricardo Sousa
-    </Typography>
-    <Typography variant="h4" mx={4}>
-      <Typewriter
-        options={{ loop: true, delay: 70, deleteSpeed: 30 }}
-        onInit={(typewriter) => {
-          typewriter
-            .pauseFor(1500)
-            .typeString("Hi there!")
-            .pauseFor(3000)
-            .deleteAll()
-            .typeString("I'm a <strong>Proud</strong> Father")
-            .pauseFor(3000)
-            .deleteChars(12)
-            .pauseFor(300)
-            .typeString("Software Developer")
-            .pauseFor(3000)
-            .deleteChars(22)
-            .pauseFor(300)
-            .typeString("ve specialized on Front-End")
-            .pauseFor(3000)
-            .deleteAll()
-            .start();
+        backgroundSize: "cover",
+        backgroundPosition: {
+          xs: "62% center",
+          sm: "65% center",
+          md: "center center",
+        },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+        textAlign: "center",
+        ...sx,
+      }}
+    >
+      <Typography variant="h1" fontWeight="700" mb={4} mx={4}>
+        Ricardo Sousa
+      </Typography>
+      <Typography variant="h4" mx={4}>
+        <Typewriter
+          options={{ loop: true, delay: 70, deleteSpeed: 30 }}
+          onInit={(typewriter) => {
+            const pause = { initial: 1500, last: 5000, long: 3000, short: 550 };
+            typewriter
+              .pauseFor(pause.initial)
+              .typeString(t("Hi there!"))
+              .pauseFor(pause.long)
+              .deleteAll()
+              .pauseFor(pause.short)
+              .typeString(t("I'm a <strong>Proud</strong> Father"))
+              .pauseFor(pause.long)
+              .deleteAll()
+              .pauseFor(pause.short)
+              .typeString(
+                t(
+                  "I love <strong>Trail Running</strong> and <strong>Calisthenics</strong>"
+                )
+              )
+              .pauseFor(pause.long)
+              .deleteAll()
+              .pauseFor(pause.short)
+              .typeString(t("I'm a <strong>Software Developer</strong>"))
+              .pauseFor(pause.long)
+              .deleteAll()
+              .pauseFor(pause.short)
+              .typeString(t("I've specialized on <strong>Front-End</strong>"))
+              .pauseFor(pause.last)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </Typography>
+      <LanguageSwitcher
+        tooltipPlacement="right-end"
+        sx={{
+          position: "absolute",
+          left: introPadding,
+          bottom: introPadding,
+          transformOrigin: "bottom left",
+          transform: "rotate(-90deg) translateY(100%)",
+          display: { xs: "none", md: "flex" },
         }}
       />
-    </Typography>
-    <LanguageSwitcher
-      sx={{
-        position: "absolute",
-        left: introPadding,
-        bottom: introPadding,
-        transformOrigin: "bottom left",
-        transform: "rotate(-90deg) translateY(100%)",
-        display: { xs: "none", md: "flex" },
-      }}
-    />
-    <VerticalSocial
-      sx={{
-        position: "absolute",
-        right: introPadding,
-        bottom: introPadding,
-        display: { xs: "none", md: "flex" },
-      }}
-    />
-  </Box>
-);
+      <VerticalSocial
+        tooltipPlacement="left"
+        sx={{
+          position: "absolute",
+          right: introPadding,
+          bottom: introPadding,
+          display: { xs: "none", md: "flex" },
+        }}
+      />
+    </Box>
+  );
+};
