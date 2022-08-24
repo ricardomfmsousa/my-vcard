3001b2e86dd608cc2538cf11d84943c51b12505e
Ricardo Sousa <r.sousa@techfirm.pt>
2022-08-24 22:33:41 +0100

    Move header link data to header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,16 @@
-import { useLocation } from "@reach/router";
+import { graphql, HeadFC, PageProps } from "gatsby";
 import * as React from "react";
 
 import { Footer } from "../components/Layout/Footer/Footer";
 import { Header } from "../components/Layout/Header/Header";
 import { Intro } from "../components/Sections/Intro/Intro";
 
-import type { HeadFC } from "gatsby";
-
-const IndexPage = () => {
-  const location = useLocation();
-  const [currentRoute, setCurrentRoute] = React.useState<string>();
-
-  React.useEffect(() => {
-    setCurrentRoute(`${location.pathname}${location.hash}`);
-  }, [location]);
-
-  const headerLinks = [
-    { name: "About", href: "/#about" },
-    { name: "Resume", href: "/#resume" },
-    { name: "Portfolio", href: "/#portfolio" },
-    { name: "Contact", href: "/#contact" },
-  ];
-
+const IndexPage: React.FC<PageProps> = (): JSX.Element => {
   const introPadding = "50px";
 
   return (
     <>
-      <Header
-        links={headerLinks}
-        currentRoute={currentRoute}
-        introPadding={introPadding}
-      />
+      <Header introPadding={introPadding} />
       <Intro introPadding={introPadding} />
       <Footer />
     </>
@@ -40,3 +20,17 @@ const IndexPage = () => {
 export default IndexPage;
 
 export const Head: HeadFC = () => <title>Home Page</title>;
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
