29821792044fd0b2bfe49a02f18f2845c491a678
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-08-25 22:29:54 +0100

    Improve code
    - Add language prefix disable feature
    - Add path prefix support

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,50 +1,49 @@
 import { globalHistory } from "@gatsbyjs/reach-router";
-import { useTheme } from "@mui/material";
+import { Link as MuiLink, styled, SxProps, useTheme } from "@mui/material";
+import { withPrefix } from "gatsby";
 import {
-  Link,
+  Link as GatsbyI18nLink,
   ReactI18NextChild,
   useI18next,
 } from "gatsby-plugin-react-i18next";
 import React from "react";
 
+import { languages } from "../../../i18n-config";
+
 export interface NavLinkProps {
   children?: ReactI18NextChild | Iterable<ReactI18NextChild>;
   to: string;
-  language?: string;
-  style?: React.CSSProperties;
+  language?: "disable" | typeof languages[number];
   onClick?: (e: React.MouseEvent) => void;
+  // Overrides default route match styling
   isActive?: boolean;
+  sx?: SxProps;
+  [rest: string]: unknown;
 }
 
 export const NavLink: React.FC<NavLinkProps> = ({
   children,
   to,
   language,
-  style,
   onClick,
   isActive,
+  sx,
+  ...rest
 }): JSX.Element => {
-  const theme = useTheme();
+  const { palette } = useTheme();
   const location = globalHistory.location;
   const { defaultLanguage, i18n } = useI18next();
-  const [isHovering, setIsHovering] = React.useState(false);
+  const [shouldHighlight, setShouldHighlight] = React.useState(false);
 
-  const currentRoute: string = React.useMemo(
-    () => `${location.pathname}${location.hash}`,
-    [location]
-  );
-
-  const localeHref: string = React.useMemo(
-    () => (i18n.language === defaultLanguage ? to : `/${i18n.language}${to}`),
-    [location]
-  );
-
-  let shouldHighlight = localeHref === currentRoute;
-
-  if (isActive !== undefined) {
-    // Override default behaviour
-    shouldHighlight = isActive;
-  }
+  React.useEffect(() => {
+    const prefixedHref = withPrefix(
+      i18n.language === defaultLanguage ? to : `/${i18n.language}${to}`
+    );
+    const currentRoute = `${location.pathname}${location.hash}`;
+    setShouldHighlight(
+      isActive !== undefined ? isActive : prefixedHref === currentRoute
+    );
+  }, [location]);
 
   const handleOnClick = React.useCallback(
     (e: React.MouseEvent) => {
@@ -58,28 +57,43 @@ export const NavLink: React.FC<NavLinkProps> = ({
     [shouldHighlight]
   );
 
-  return (
-    <Link
+  const linkStyle = {
+    textDecoration: "none",
+    color: palette.text.primary,
+    cursor: "pointer",
+    ...(shouldHighlight && {
+      cursor: "default",
+      fontWeight: "bold",
+      textShadow: `0 0 1px ${palette.text.primary}`,
+    }),
+    ...(!shouldHighlight && {
+      "&:hover": {
+        color: palette.text.disabled,
+      },
+    }),
+  };
+
+  const StyledGatsbyI18nLink = styled(GatsbyI18nLink)(linkStyle);
+  const href = withPrefix(to);
+
+  return language === "disable" ? (
+    <MuiLink
+      href={href}
+      onClick={handleOnClick}
+      sx={{ ...linkStyle, ...sx }}
+      {...rest}
+    >
+      {children}
+    </MuiLink>
+  ) : (
+    <StyledGatsbyI18nLink
       to={to}
       language={language}
       onClick={handleOnClick}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-      style={{
-        color:
-          !shouldHighlight && isHovering
-            ? theme.palette.text.disabled
-            : theme.palette.text.primary,
-        textDecoration: "none",
-        cursor: shouldHighlight ? "default" : "pointer",
-        ...(shouldHighlight && {
-          fontWeight: "bold",
-          textShadow: `0 0 1px ${theme.palette.text.primary}`,
-        }),
-        ...style,
-      }}
+      sx={sx}
+      {...rest}
     >
       {children}
-    </Link>
+    </StyledGatsbyI18nLink>
   );
 };
