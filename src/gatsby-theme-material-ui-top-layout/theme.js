8e1ba34d7f7e4f52d075a913ed43d548e7e9dc64
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-08-27 00:24:00 +0100

    Update theme font sizes and weights

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -4,11 +4,23 @@ const theme = responsiveFontSizes(
   createTheme({
     typography: {
       fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontWeightLight: 200,
+      fontWeightRegular: 400,
+      fontWeightMedium: 700,
+      h1: {
+        fontSize: "4.5rem",
+        fontWeight: "bold",
+      },
+      h2: {
+        fontSize: "3rem",
+        fontWeight: "bold",
+      },
     },
     palette: {
       mode: "dark",
       background: {
         default: "#121212",
+        darker: "#020202",
       },
     },
   })
