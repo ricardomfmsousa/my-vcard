a3b490dfa01eb4d450bc100964fec8ed69f0523c
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-01 00:40:09 +0100

    Add ScrollTop component

---
+++ src/components/ScrollTop/ScrollTop.stories.tsx
@@ -0,0 +1,32 @@
+import { Stack, Typography } from "@mui/material";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import { ScrollTop as ScrollTopComponent } from "./ScrollTop";
+
+export default {
+  title: "Design System/ScrollTop",
+  component: ScrollTopComponent,
+} as ComponentMeta<typeof ScrollTopComponent>;
+
+const text = Array(20)
+  .fill(0)
+  .map(() => "Scroll down and look at the bottom right!");
+
+export const ScrollTop: ComponentStory<typeof ScrollTopComponent> = (args) => (
+  <Stack>
+    {text.map((t) => (
+      <Typography>
+        {t}
+        <br />
+        <br />
+      </Typography>
+    ))}
+    <ScrollTopComponent {...args} />
+  </Stack>
+);
+
+ScrollTop.args = {
+  bottom: "50px",
+  right: "50px",
+};
