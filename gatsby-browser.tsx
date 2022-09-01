4f359c56429281ffd82f18bc22ebad049cd112b6
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-01 02:07:44 +0100

    Add console info text

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -16,3 +16,38 @@ export const wrapPageElement = ({ element }) => {
     </ThemeProvider>
   );
 };
+
+const consoleStyles = {
+  h1: `
+    color: ${theme.palette.primary.main};
+    font-family: monospace;
+    font-size: 60px;
+    font-weight: bold;
+`,
+  h2: `
+    color: ${theme.palette.primary.main};
+    font-family: monospace;
+    font-size: 40px;
+    font-weight: bold;
+`,
+  p: `
+    color: ${theme.palette.primary.main};
+    font-family: monospace;
+    font-size: 15px;
+`,
+  pb: `
+    color: ${theme.palette.primary.main};
+    font-family: monospace;
+    font-size: 15px;
+    font-weight: bold;
+`,
+};
+
+console.log("%cHI THERE!", consoleStyles.h1);
+console.log("%cWelcome to my vCard website.\n", consoleStyles.h2);
+console.log("%cYou can check my source code at Github:", consoleStyles.p);
+console.log("%chttps://github.com/ricardomfmsousa/my-vcard", consoleStyles.pb);
+console.log(
+  "%cFeel free to open an issue if you've found the slightest bug (thanks in advance :)",
+  consoleStyles.p
+);
