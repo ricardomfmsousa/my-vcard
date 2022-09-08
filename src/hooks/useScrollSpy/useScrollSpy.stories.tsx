9a4b7721fe7d910be2e9059a7f42de42f4e8b562
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-08 20:24:58 +0100

    Add useScrollSpy hook

---
+++ src/hooks/useScrollSpy/useScrollSpy.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, Story } from "@storybook/react";
+import React from "react";
+
+import { useScrollSpy as useScrollSpyHook } from "./useScrollSpy";
+
+export default {
+  title: "Hooks/useScrollSpy",
+  // component: TODO,
+} as Meta<typeof useScrollSpy>;
+
+export const useScrollSpy: Story<typeof useScrollSpyHook> = (args) => <>TODO</>;
+
+useScrollSpy.args = {};
