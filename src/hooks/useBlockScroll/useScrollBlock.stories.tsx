d9062ece18148e89ada9cdf127d3be6e69cd7308
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-08 20:23:48 +0100

    Add useScrollBlock hook

---
+++ src/hooks/useBlockScroll/useScrollBlock.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, Story } from "@storybook/react";
+import React from "react";
+
+import { useScrollBlock as useScrollBlockHook } from "./useScrollBlock";
+
+export default {
+  title: "Hooks/useScrollBlock",
+  // component: TODO,
+} as Meta<typeof useScrollBlockHook>;
+
+export const useScrollBlock: Story<typeof useScrollBlockHook> = (args) => (
+  <>TODO</>
+);
+
+useScrollBlock.args = {};
