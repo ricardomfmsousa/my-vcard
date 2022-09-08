1cc97b9c48fa341a67dce3b01239069fb44e7301
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-08 20:19:14 +0100

    Add technical overview story
    - Add md file import config
    - Import from README.md

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,18 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-docs",
+      options: { transcludeMarkdown: true },
+    },
+    "@storybook/addon-a11y",
+    "@storybook/addon-storysource",
+    "@storybook/addon-links",
     "@storybook/addon-interactions",
+    "@a110/storybook-expand-all",
     "storybook-addon-gatsby",
     "storybook-dark-mode",
-    "@a110/storybook-expand-all",
     "storybook-react-i18next",
   ],
   framework: "@storybook/react",
