0c55c06a03b3622047cfbfb1c13f996cb0b10230
Ricardo Sousa <ricardomfmsousa@gmail.com>
2022-09-22 22:16:44 +0100

    Remove google analytics plugin, improve variables

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,24 +5,27 @@ import { defaultLanguage, languages, translationPath } from "./i18n-config";
 import type { GatsbyConfig } from "gatsby";
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const siteUrl = "https://ricardomfmsousa.github.io/my-vcard/";
+const siteMetadata = {
+  title: "Ricardo Sousa's vCard",
+  shortTitle: "RS vCard",
+  description: "Ricardo Sousa's presentation website",
+  siteUrl: "https://ricardomfmsousa.github.io/my-vcard/",
+  license:
+    "https://raw.githubusercontent.com/ricardomfmsousa/my-vcard/main/LICENSE",
+  author: {
+    name: "Ricardo Sousa",
+    github: "https://github.com/ricardomfmsousa",
+    strava: "https://strava.com/athletes/ricardomfmsousa",
+    linkedIn: "https://www.linkedin.com/in/ricardomfmsousa",
+    discord: "https://discordapp.com/users/377100083990102016",
+  },
+};
 
 const config: GatsbyConfig = {
   pathPrefix: "/my-vcard",
-  siteMetadata: {
-    title: "Ricardo Sousa's vCard",
-    siteUrl,
-    license:
-      "https://raw.githubusercontent.com/ricardomfmsousa/my-vcard/main/LICENSE",
-    author: {
-      name: "Ricardo Sousa",
-      github: "https://github.com/ricardomfmsousa",
-      strava: "https://strava.com/athletes/ricardomfmsousa",
-      linkedIn: "https://www.linkedin.com/in/ricardomfmsousa",
-      discord: "https://discordapp.com/users/377100083990102016",
-    },
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  siteMetadata,
+  // More easily incorporate content into your pages through automatic
+  // TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
@@ -35,18 +38,12 @@ const config: GatsbyConfig = {
     "gatsby-plugin-emotion",
     "gatsby-theme-material-ui",
     {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "3989290945",
-      },
-    },
-    {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "Ricardo Sousa's presentation website",
-        short_name: "vCard",
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortTitle,
         start_url: "/",
-        background_color: "#663399",
+        background_color: "#020202",
         theme_color: "#ffffff",
         display: "standalone",
         icon: "src/images/favicon.png",
@@ -78,17 +75,16 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-react-i18next`,
       options: {
-        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        // Name given to `gatsby-source-filesystem` plugin.
+        localeJsonSourceName: `locale`,
         languages,
         defaultLanguage,
-        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
-        siteUrl,
-        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+        siteUrl: siteMetadata.siteUrl,
         trailingSlash: "always",
-        // you can pass any i18next options
+        // Pass any i18next options
         i18nextOptions: {
           interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
+            escapeValue: false,
           },
           keySeparator: false,
           nsSeparator: false,
